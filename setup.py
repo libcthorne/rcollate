1282176bc8f8b2261eb34647489e5901036c9ffc
Christopher Thorne <libcthorne@gmail.com>
2017-06-22 22:01:31 +0100

    (#48) Move requirements.txt contents into setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     packages=['rcollate'],
     include_package_data=True,
     install_requires=[
-        'flask',
+        'APScheduler==3.3.1',
+        'emails==0.5.14',
+        'Flask==0.12.2',
+        'Jinja2==2.9.6',
+        'praw==4.5.1',
     ],
 )
