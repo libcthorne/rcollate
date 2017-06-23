079d93e9e0f3c5f4ef3e56b76138b46f255757db
Christopher Thorne <libcthorne@gmail.com>
2017-06-23 20:21:29 +0100

    (#45) Add schema validation for config files

--- rcollate/config.py
+++ rcollate/config.py
@@ -1,6 +1,60 @@
 import json
+import sys
+
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 from rcollate import resources
 
-settings = resources.read_json_file("config/settings.json")
-secrets = resources.read_json_file("config/secrets.json")
+SETTINGS_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "user_agent": {"type": "string"},
+        "sender_name": {"type": "string"},
+        "sender_email": {"type": "string"},
+        "smtp_host": {"type": "string"},
+        "smtp_timeout": {"type": "integer"},
+        "app_url": {"type": "string"},
+    },
+    "required": [
+        "user_agent",
+        "sender_name",
+        "sender_email",
+        "smtp_host",
+        "smtp_timeout",
+        "app_url",
+    ],
+    "additionalProperties": False,
+}
+
+SECRETS_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "client_id": {"type": "string"},
+        "client_secret": {"type": "string"},
+        "admin_username": {"type": "string"},
+        "admin_password": {"type": "string"},
+    },
+    "required": [
+        "client_id",
+        "client_secret",
+        "admin_username",
+        "admin_password",
+    ],
+}
+
+def read_config_file(path, schema):
+    contents = resources.read_json_file(path)
+
+    try:
+        validate(contents, schema)
+        return contents
+    except ValidationError as e:
+        print("Error reading {}: {}".format(
+            path,
+            e.message,
+        ))
+        sys.exit(1)
+
+settings = read_config_file("config/settings.json", SETTINGS_SCHEMA)
+secrets = read_config_file("config/secrets.json", SECRETS_SCHEMA)
