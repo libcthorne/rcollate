7b5c6e59ba89426f6713e21577426ed62d466d22
Christopher Thorne <libcthorne@gmail.com>
2017-07-01 15:31:00 +0100

    (#56) Fix quotes in config.py

--- rcollate/config.py
+++ rcollate/config.py
@@ -7,43 +7,43 @@ from jsonschema.exceptions import ValidationError
 from rcollate import resources
 
 SETTINGS_SCHEMA = {
-    "type": "object",
-    "properties": {
-        "user_agent": {"type": "string"},
-        "sender_name": {"type": "string"},
-        "sender_email": {"type": "string"},
-        "smtp_host": {"type": "string"},
-        "smtp_timeout": {"type": "integer"},
-        "app_url": {"type": "string"},
+    'type': 'object',
+    'properties': {
+        'user_agent': {'type': 'string'},
+        'sender_name': {'type': 'string'},
+        'sender_email': {'type': 'string'},
+        'smtp_host': {'type': 'string'},
+        'smtp_timeout': {'type': 'integer'},
+        'app_url': {'type': 'string'},
     },
-    "required": [
-        "user_agent",
-        "sender_name",
-        "sender_email",
-        "smtp_host",
-        "smtp_timeout",
-        "app_url",
+    'required': [
+        'user_agent',
+        'sender_name',
+        'sender_email',
+        'smtp_host',
+        'smtp_timeout',
+        'app_url',
     ],
-    "additionalProperties": False,
+    'additionalProperties': False,
 }
 
 SECRETS_SCHEMA = {
-    "type": "object",
-    "properties": {
-        "client_id": {"type": "string"},
-        "client_secret": {"type": "string"},
-        "admin_username": {"type": "string"},
-        "admin_password": {"type": "string"},
-        "session_secret_key": {"type": "string"},
+    'type': 'object',
+    'properties': {
+        'client_id': {'type': 'string'},
+        'client_secret': {'type': 'string'},
+        'admin_username': {'type': 'string'},
+        'admin_password': {'type': 'string'},
+        'session_secret_key': {'type': 'string'},
     },
-    "required": [
-        "client_id",
-        "client_secret",
-        "admin_username",
-        "admin_password",
-        "session_secret_key",
+    'required': [
+        'client_id',
+        'client_secret',
+        'admin_username',
+        'admin_password',
+        'session_secret_key',
     ],
-    "additionalProperties": False,
+    'additionalProperties': False,
 }
 
 def read_config_file(path, schema):
@@ -59,5 +59,5 @@ def read_config_file(path, schema):
         ))
         sys.exit(1)
 
-settings = read_config_file("config/settings.json", SETTINGS_SCHEMA)
-secrets = read_config_file("config/secrets.json", SECRETS_SCHEMA)
+settings = read_config_file('config/settings.json', SETTINGS_SCHEMA)
+secrets = read_config_file('config/secrets.json', SECRETS_SCHEMA)
