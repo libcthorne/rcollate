679fb305cf93a1dc785553450390ff6ec34a5afa
Christopher Thorne <libcthorne@gmail.com>
2017-07-08 20:19:28 +0100

    (#32) Add basic auth test

--- tests/test_rcollate.py
+++ tests/test_rcollate.py
@@ -1,3 +1,4 @@
+import base64
 import unittest
 
 import rcollate
@@ -9,6 +10,23 @@ class RCollateTestCase(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    @property
+    def auth_headers(self):
+        return {
+            'Authorization': 'Basic ' + base64.b64encode(
+                b"test_user:test_password"
+            ).decode('ascii')
+        }
+
+class TestJobsIndex(RCollateTestCase):
+    def test_jobs_index_no_auth(self):
+        rv = self.app.get('/jobs/')
+        self.assertEqual(rv.status_code, 401)
+
+    def test_jobs_index_auth(self):
+        rv = self.app.get('/jobs/', headers=self.auth_headers)
+        self.assertEqual(rv.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
