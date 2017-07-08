a7296189d9d398692011677981746f3550b92a56
Christopher Thorne <libcthorne@gmail.com>
2017-07-08 21:05:50 +0100

    (#32) Add new job page tests

--- tests/test_rcollate.py
+++ tests/test_rcollate.py
@@ -3,13 +3,25 @@ import unittest
 
 import rcollate
 
+VALID_SUBREDDITS = ['hello', 'world']
+VALID_SUBREDDIT = VALID_SUBREDDITS[0]
+INVALID_SUBREDDIT = 'test'
+
+def mock_subreddit_exists(subreddit):
+    return subreddit in VALID_SUBREDDITS
+
 class RCollateTestCase(unittest.TestCase):
     def setUp(self):
         rcollate.app.testing = True
+        rcollate.app.config['WTF_CSRF_ENABLED'] = False
         self.app = rcollate.app.test_client()
 
+        self.mocks = {}
+        self.mocks['subreddit_exists'] = rcollate.reddit.subreddit_exists
+        rcollate.reddit.subreddit_exists = mock_subreddit_exists
+
     def tearDown(self):
-        pass
+        rcollate.reddit.subreddit_exists = self.mocks['subreddit_exists']
 
     @property
     def auth_headers(self):
@@ -38,6 +50,34 @@ class TestJobsIndexPage(RCollateTestCase):
             rv = self.app.get('/jobs/', headers=self.auth_headers)
             self.assertTrue('Job ' in str(rv.data))
             rcollate.rcollate.delete_job(job.job_key)
+
+class TestJobsNewPage(RCollateTestCase):
+    def test_get_status_code(self):
+        rv = self.app.get('/jobs/new/')
+        self.assertEqual(rv.status_code, 200)
+
+    def test_post_invalid_subreddit(self):
+        rv = self.app.post('/jobs/new/', data={
+            'subreddit': INVALID_SUBREDDIT,
+            'target_email': 'test@test.com',
+        })
+        self.assertTrue('Please enter a valid subreddit' in str(rv.data))
+
+    def test_post_invalid_email(self):
+        rv = self.app.post('/jobs/new/', data={
+            'subreddit': VALID_SUBREDDIT,
+            'target_email': 'test',
+        })
+        self.assertTrue('Please enter a valid email' in str(rv.data))
+
+    def test_post_valid(self):
+        rv = self.app.post('/jobs/new/', data={
+            'subreddit': VALID_SUBREDDIT,
+            'target_email': 'test@test.com',
+        })
+        self.assertEqual(rv.status_code, 302)
+        self.assertRegex(rv.location, '/jobs/[0-9a-zA-Z]+')
+
 
 if __name__ == "__main__":
     unittest.main()
