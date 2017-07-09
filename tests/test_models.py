fb3ba2186c7efc482baaa720be6d19e479986fb6
Christopher Thorne <libcthorne@gmail.com>
2017-07-09 10:54:35 +0100

    (#32) Add model creation test

---
+++ tests/test_models.py
@@ -0,0 +1,16 @@
+import unittest
+
+from rcollate import models
+
+class JobModelTest(unittest.TestCase):
+    def test_job_model_create_no_key(self):
+        job = models.Job(
+            subreddit='hello',
+            target_email='test@test.com',
+            cron_trigger={'hour': 6},
+        )
+
+        self.assertEqual(
+            str(job),
+            '<Job(job_key=None, subreddit=hello)>'
+        )
