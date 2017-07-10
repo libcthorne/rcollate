0fcf47743da67d6c401b80e5e5111d16006827c5
Christopher Thorne <libcthorne@gmail.com>
2017-07-10 19:57:18 +0100

    (#67) Mock email sending in mailer test

--- tests/test_mailer.py
+++ tests/test_mailer.py
@@ -1,17 +1,37 @@
 import unittest
+from unittest.mock import patch
 
 from rcollate.mailer import Mailer
 
+class ResponseMock(object):
+    def __init__(self, status_code):
+        self.status_code = status_code
+        self.error = status_code
+
+class MessageMock(object):
+    def __init__(self, *_args, **_kwargs):
+        pass
+
+class ValidMessageMock(MessageMock):
+    def send(self, *args, **kwargs):
+        return ResponseMock(250)
+
+class InvalidMessageMock(MessageMock):
+    def send(self, *args, **kwargs):
+        return ResponseMock(0)
+
 class MailerTest(unittest.TestCase):
-    def test_valid_send(self):
-        mailer = Mailer(
+    def setUp(self):
+        self.mailer = Mailer(
             smtp_host='localhost',
             smtp_timeout=5,
             sender_name='TEST',
             sender_email='test@test.com',
         )
 
-        success = mailer.send_threads(
+    @patch('rcollate.mailer.emails.html', ValidMessageMock)
+    def test_valid_send(self):
+        success = self.mailer.send_threads(
             r_threads=[],
             target_email='test2@test.com',
             subreddit='test',
@@ -20,15 +40,9 @@ class MailerTest(unittest.TestCase):
 
         self.assertTrue(success)
 
+    @patch('rcollate.mailer.emails.html', InvalidMessageMock)
     def test_invalid_send(self):
-        mailer = Mailer(
-            smtp_host='INVALIDHOST[]~',
-            smtp_timeout=5,
-            sender_name='TEST',
-            sender_email='test@test.com',
-        )
-
-        success = mailer.send_threads(
+        success = self.mailer.send_threads(
             r_threads=[],
             target_email='test2@test.com',
             subreddit='test',
